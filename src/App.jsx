e1ab1df445b482214b08d18c6c521cf20ffce5fe
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-04-08 20:28:45 +0600

    added home about page and error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <h1>Welcome to HeroGadget</h1>
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, ErrorPage, Home } from "./Pages";
+import { Footer, Header } from "./components";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
